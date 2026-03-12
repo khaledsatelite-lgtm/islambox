af388a701364f384b2486d8cfda7a00c633ec9ae
khaledsatelite-lgtm <khaledsatelite@gmail.com>
2026-03-12 03:03:20 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'islambox-v1';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(['./islam-box-tech.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
