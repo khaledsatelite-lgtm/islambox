77a4f09a29a6fe6cc993e7146d1cc3522f7eba5c
khaledsatelite-lgtm <khaledsatelite@gmail.com>
2026-03-13 04:34:20 +0100

    Update fetch strategy and cache deletion logic

--- sw.js
+++ sw.js
@@ -7,14 +7,17 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.map(function(k){ return caches.delete(k); })
-      );
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
     }).then(function(){ return self.clients.claim(); })
   );
 });
 
 self.addEventListener('fetch', function(e) {
-  // Always fetch from network - no cache
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request.url + '?v=' + Date.now()).then(function(response){
+      return response;
+    }).catch(function(){
+      return caches.match(e.request);
+    })
+  );
 });
